34a59488d14c34944b1b83364328001df93532c9
yavuz samet kan <yssk.personal@gmail.com>
2025-05-21 20:31:17 +0300

    created to new project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+      <div className="text-4xl text-green-500 font-bold text-center mt-10">
+        Tailwind çalışıyor!
+      </div>
+  )
+}
+
+export default App
